23536c1867a37ca4cb92448cc9b716a4fb4d9d81
Anton Kvartalnyi <anton.kvart@gmail.com>
2018-12-06 17:32:12 +0200

    Initial commit

---
+++ src/ua/hillel/kvartalnyi/lesson02/task01/task01.java
@@ -0,0 +1,22 @@
+package ua.hillel.kvartalnyi.lesson02.task01;
+
+public class task01 {
+    public static void main(String[] args) {
+        int[] mas = new int[10];
+        mas[0] = 8;
+        mas[1] = 2;
+        mas[2] = 1;
+        mas[3] = 6;
+        mas[4] = 3;
+        mas[5] = 8;
+        mas[6] = 5;
+        mas[7] = 6;
+        mas[8] = 0;
+        mas[9] = 9;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(mas[i]);
+        }
+    }
+
+}
+ 
